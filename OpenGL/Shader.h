c115b81a2b364e92ac27d30becaa9b5c47b5ca68
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-09-10 15:18:51 -0400

    Shader.h complete

--- OpenGL/Shader.h
+++ OpenGL/Shader.h
@@ -3,8 +3,33 @@
 
 #include "StandardIncludes.h"
 
-class Shader
-{
+class Shader {
+
+public:
+	// Contructor / Destructor
+	Shader();
+	virtual ~Shader();
+	
+	// Accessors
+	GLuint GetProgramID() const { return m_programID; }
+	GLuint GetAttrVertices() const { return m_attrVertices; }
+
+	// Methods
+	void LoadShaders(const char* _vertexFilePath, const char* _fragmentFilePath);
+	void Cleanup();
+
+private:
+	//Methods
+	void CreateShaderProgram(const char* _vertexFilePath, const char* _fragmentFilePath);
+	GLuint LoadShaderFile(const char* _filePath, GLenum _type);
+	void LoadAttributes();
+	void EvaluateShader(int _infoLength, GLuint _id);
+
+	// Members
+	GLuint m_programID; // ID of the shader program
+	GLuint m_attrVertices; // Handle for the attribute vertex buffer
+	GLuint m_result = GL_FALSE;
+	int m_infoLogLength;
 };
 
 #endif // !SHADER_H
