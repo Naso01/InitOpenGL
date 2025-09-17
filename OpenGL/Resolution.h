6f4fe92495601dcc106a3b86a464b36f8e8baebb
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-09-17 16:56:17 -0400

    Resolution Struct Complete

---
+++ OpenGL/Resolution.h
@@ -0,0 +1,21 @@
+#ifndef RESOLUTION_H
+#define RESOLUTION_H
+
+struct Resolution {
+	
+	int width;
+	int height;
+
+	Resolution(int w, int h) : width(w), height(h) {}
+
+};
+
+
+
+
+
+
+
+
+
+#endif // RESOLUTION_H
