77092cf7d069a031fc6e8964574209d70b9288c1
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-09-23 23:04:06 -0400

    fixed the issue (was cause by the tool window)

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -10,9 +10,9 @@ GameController::GameController() {
 
 void GameController::Initialize() {
 
-	GLFWwindow* window = WindowController::GetInstance().GetWindow(); // Call this first, as it creates a window required by GLEW
+	GLFWwindow* glfwWindow = WindowController::GetInstance().GetWindow(); // Call this first, as it creates a window required by GLEW
 	M_ASSERT(glewInit() == GLEW_OK, "Failed to initialize GLEW."); // Initialize GLEW
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE); // Ensure we can capture the escape key
+	glfwSetInputMode(glfwWindow, GLFW_STICKY_KEYS, GL_TRUE); // Ensure we can capture the escape key
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Black background
 
 	// Create a default perspective camera
@@ -22,8 +22,8 @@ void GameController::Initialize() {
 void GameController::RunGame() {
 
 	//Show the C++/CLI tool window
-	OpenGL::ToolWindow^ window = gcnew OpenGL::ToolWindow();
-	window->Show();
+	//OpenGL::ToolWindow^ window = gcnew OpenGL::ToolWindow();
+	//window->Show();
 
 	//Create and compile our GLSL program from the shaders
 	m_shader = Shader();
@@ -35,6 +35,7 @@ void GameController::RunGame() {
 	do {
 		System::Windows::Forms::Application::DoEvents(); // Handle Windows events
 
+		
 		//Checkbox states from the tool window
 		GLint loc = glGetUniformLocation(m_shader.GetProgramID(), "RenderRedChannel");
 		glUniform1i(loc, (int)OpenGL::ToolWindow::RenderRedChannel);
@@ -44,7 +45,7 @@ void GameController::RunGame() {
 		
 		loc = glGetUniformLocation(m_shader.GetProgramID(), "RenderBlueChannel");
 		glUniform1i(loc, (int)OpenGL::ToolWindow::RenderBlueChannel);
-
+		
 
 		glClear(GL_COLOR_BUFFER_BIT); //Clear the screen
 		m_mesh.Render(m_camera.GetProjection() * m_camera.GetView()); // Gives mesh View and Projection matrices
