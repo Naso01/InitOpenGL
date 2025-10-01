923c11abacd3b1b34b334bb8d0327b0ca8c03c6f
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-09-30 20:43:53 -0400

    texture class created vertex and fragment shader updated to implement texture

---
+++ OpenGL/Texture.h
@@ -0,0 +1,28 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "StandardIncludes.h"
+
+class Texture
+{
+public:
+	//Constructors / Destructors
+	Texture();
+	virtual ~Texture() { }
+
+	//Accessors
+	GLuint GetTexture() { return m_texture; }
+
+	//Methods
+	void LoadTexture(string _fileName);
+	void Cleanup();
+
+private:
+	//Members
+	int m_width;
+	int m_height;
+	int m_channels;
+	GLuint m_texture;
+};
+
+#endif // TEXTURE_H
