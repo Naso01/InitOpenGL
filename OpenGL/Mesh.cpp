b69b9e877df99fb0c0548fea2b4ce8bd9371f5c5
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-10-11 23:36:57 -0400

    code clean up

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -33,38 +33,6 @@ void Mesh::Create(Shader* _shader) {
 	m_texture.LoadTexture("../Assets/Textures/Wood.jpg");
 	m_texture2 = Texture();
 	m_texture2.LoadTexture("../Assets/Textures/Emoji.jpg");
-	glm::vec4 lightDir = glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
-
-	/*
-	glm::mat3 yuvMatrix = glm::mat3(
-		0.299f, 0.587f, 0.114f,
-		-0.14713f, -0.28886f, 0.436f,
-		0.615f, -0.51499f, -0.10001f
-	);
-	glm::vec3 yuv = glm::vec3(0.0f, 0.0f, 0.0f) * yuvMatrix;
-
-	yuv.r = 0;
-	yuv.g = 0;
-	yuv.b = -0.3f;
-
-
-	glm::mat3 rgbMatrix = glm::mat3(
-		1, 0, 1.13983f,
-		1, -0.39465f, -0.58060f,
-		1, 2.03211f, 0
-	);
-
-	glm::vec3 rgb =  glm::vec3(0, +0.2, -0.2) * rgbMatrix ;
-
-	m_vertexData = {
-		// Position 			RGB Color 		Texture Coords 
-		50.0f, 50.0f, 0.0f,		 1 + rgb.r, rgb.g, rgb.b,	1.0f, 1.0f,	// top-right
-		50.0f, -50.0f, 0.0f,	 rgb.r, 1+ rgb.g, rgb.b,	1.0f, 0.0f,	// bottom-right
-		-50.0f, -50.0f, 0.0f,	 rgb.r, rgb.g, 1 +rgb.b,	0.0f, 0.0f,	// bottom-left
-		-50.0f, 50.0f, 0.0f,	 1 + rgb.r, 1 + rgb.g, 1 + rgb.b,	0.0f, 1.0f	// top-left
-	};
-	*/
-/**/
 
 	m_vertexData = {
 		
