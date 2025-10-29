fb34a6b5a97696069f654862409b7b933209f2c8
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-10-29 13:10:23 -0400

    added SetTextureSampler() to shader class

--- OpenGL/Shader.cpp
+++ OpenGL/Shader.cpp
@@ -18,6 +18,16 @@ void Shader:: Cleanup() {
 	glDeleteProgram(m_programID);
 }
 
+void Shader::SetTextureSampler(const char* _name, GLuint _texUnit, int _texUnitID, int _value) {
+
+	GLint loc = glGetUniformLocation(m_programID, _name);
+	if (loc != -1) {
+		glActiveTexture(_texUnit);
+		glBindTexture(GL_TEXTURE_2D, _value);
+		glUniform1f(loc, _texUnitID);
+	}
+}
+
 void Shader::SetVec3(const char* _name, glm::vec3 _value) {
 	GLint loc = glGetUniformLocation(m_programID, _name);
 	if (loc != -1) {
