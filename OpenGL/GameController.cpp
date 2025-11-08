02fffb35f68279956f2a7b6c8662cd1db2eb03b2
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-11-07 22:47:05 -0500

    multiple-Lights work

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -40,19 +40,22 @@ void GameController::RunGame() {
 	m_shaderDiffuse.LoadShaders("Diffuse.vertexshader", "Diffuse.fragmentshader");
 
 	//Create meshes
-	m_meshLight = Mesh();
-	m_meshLight.Create(&m_shaderColor);
-	m_meshLight.SetPosition({ 0.5f, 0.0f, -0.5f });
-	m_meshLight.SetScale({ 0.1f, 0.1f, 0.1f });
+	for (int count = 0; count < 4; count++) {
+
+		Mesh m = Mesh();
+		m.Create(&m_shaderColor);
+		m.SetPosition({ 0.5f + (float)count / 10.0f, 0.0f, -0.5f });
+		m.SetColor({ glm::linearRand(0.0f, 1.0f), glm::linearRand(0.0f, 1.0f),  glm::linearRand(0.0f, 1.0f) });
+		m.SetScale({ 0.1f, 0.1f, 0.1f });
+		Mesh::Lights.push_back(m);
+	}
 	
-	for (int col = 0; col < 2; col++) {
+	for (int col = 0; col < 10; col++) {
 
 		for (int count = 0; count < 10; count++) {
 		
 			Mesh box = Mesh();
 			box.Create(&m_shaderDiffuse);
-			box.SetLightColor({ 1.0f, 1.0f, 1.0f });
-			box.SetLightPosition(m_meshLight.GetPosition());
 			box.SetCameraPosition(m_camera.GetPosition());
 			box.SetScale({ 0.1f, 0.1f , 0.1f });
 			box.SetPosition({ 0.0f, -0.5f + (float)count / 10.0f, -0.2f + (float)col / 10.0f });
@@ -61,30 +64,26 @@ void GameController::RunGame() {
 	}
 	do {
 		
-		//Checkbox states from the tool window 
-		/*
-		GLint loc = glGetUniformLocation(m_shader.GetProgramID(), "RenderRedChannel");
-		glUniform1i(loc, (int)OpenGL::ToolWindow::RenderRedChannel);
-		
-		loc = glGetUniformLocation(m_shader.GetProgramID(), "RenderGreenChannel");
-		glUniform1i(loc, (int)OpenGL::ToolWindow::RenderGreenChannel);
-		
-		loc = glGetUniformLocation(m_shader.GetProgramID(), "RenderBlueChannel");
-		glUniform1i(loc, (int)OpenGL::ToolWindow::RenderBlueChannel);
-		*/
-
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the screen
+
+		//Box
 		for (unsigned int count = 0; count < m_meshBoxes.size(); count++) {
 			m_meshBoxes[count].Render(m_camera.GetProjection() * m_camera.GetView());
 		}
-		m_meshLight.Render(m_camera.GetProjection() * m_camera.GetView()); 
+		for (unsigned int count = 0; count < Mesh::Lights.size(); count++) {
+			Mesh::Lights[count].Render(m_camera.GetProjection() * m_camera.GetView());
+		}
+
+		
 		glfwSwapBuffers(WindowController::GetInstance().GetWindow()); // Swap the front and back buffers
 		glfwPollEvents();
 
 	} while (glfwGetKey(WindowController::GetInstance().GetWindow(), GLFW_KEY_ESCAPE) != GLFW_PRESS && // Check if the ESC key was pressed
 		glfwWindowShouldClose(WindowController::GetInstance().GetWindow()) == 0); // Check if the window was closed
 
-	m_meshLight.Cleanup();
+	for (unsigned int count = 0; count < Mesh::Lights.size(); count++) {
+		Mesh::Lights[count].Cleanup();
+	}
 	for (unsigned int count = 0; count < m_meshBoxes.size(); count++) {
 		m_meshBoxes[count].Cleanup();
 	}
--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -141,19 +141,21 @@ void Mesh::SetShaderVariables(glm::mat4 _pv) {
 	m_shader->SetVec3("CameraPosition", m_cameraPosition);
 
 	//Configure lighting
-	m_shader->SetVec3("light.color", m_lightColor);
-	m_shader->SetFloat("light.constant", 1.0f);
-	m_shader->SetFloat("light.linear", 0.09f);
-	m_shader->SetFloat("light.quadratic", 0.032f);
+	for (unsigned int i = 0; i < Lights.size(); i++) {
 
-	m_shader->SetVec3("light.ambientColor", { 0.1f, 0.1f, 0.1f });
-	m_shader->SetVec3("light.diffuseColor", { 1.0f, 1.0f, 1.0f });
-	m_shader->SetVec3("light.specularColor", { 3.0f, 3.0f, 3.0f });
+		m_shader->SetFloat(Concat("light[", i, "].constant").c_str(), 1.0f);
+		m_shader->SetFloat(Concat("light[", i, "].linear").c_str(), 0.09f);
+		m_shader->SetFloat(Concat("light[", i, "].quadratic").c_str(), 0.032f);
 
-	m_shader->SetVec3("light.position", m_lightPosition);
-	m_shader->SetVec3("light.direction", glm::normalize(glm::vec3({0, 0, 0}) - m_lightPosition));
-	m_shader->SetFloat("light.coneAngle", glm::radians(15.0f));
-	m_shader->SetFloat("light.falloff", 100);
+		m_shader->SetVec3(Concat("light[", i, "].ambientColor").c_str(), { 0.1f, 0.1f, 0.1f });
+		m_shader->SetVec3(Concat("light[", i, "].diffuseColor").c_str(), Lights[i].GetColor());
+		m_shader->SetVec3(Concat("light[", i, "].specularColor").c_str(), { 3.0f, 3.0f, 3.0f });
+
+		m_shader->SetVec3(Concat("light[", i, "].position").c_str(), Lights[i].GetPosition());
+		m_shader->SetVec3(Concat("light[", i, "].direction").c_str(), glm::normalize(glm::vec3({ 0.0f + i * 0.1f, 0, 0.0f + i * 0.1f }) - Lights[i].GetPosition()));
+		m_shader->SetFloat(Concat("light[", i, "].coneAngle").c_str(), glm::radians(5.0f));
+		m_shader->SetFloat(Concat("light[", i, "].falloff").c_str(), 200);
+	}
 
 	//Configure material
 	m_shader->SetFloat("material.specularStrength", 8);
