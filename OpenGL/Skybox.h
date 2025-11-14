244ce1d56118bc0bf2d1e8a583305e8786cf1e9a
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-11-14 17:21:36 -0500

    added skybox class

---
+++ OpenGL/Skybox.h
@@ -0,0 +1,29 @@
+#ifndef SKYBOX_H
+#define SKYBOX_H
+
+#include "Mesh.h"
+
+class Skybox {
+public:
+	//Constructors / Destructors
+	Skybox();
+	virtual ~Skybox();
+
+	//Methods
+	void Create(Shader* _shader, string _file, vector<string> _faces);
+	void Cleanup();
+	void Render(glm::mat4 _pv);
+
+private:
+	//Methods
+	void SetShaderVariables(glm::mat4 _pv);
+	void BindAttributes();
+
+	//Members
+	Shader* m_shader;
+	Texture m_texture;
+	GLuint m_vertexBuffer; // GPU buffer
+	vector<GLfloat> m_vertexData; // Store vertex data in RAM
+};
+
+#endif //!SKYBOX_H
